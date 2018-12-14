93933069ed050b04620a330a704dc2b6fd0a1d39
Nipuncp <40162053+Nipuncp@users.noreply.github.com>
2018-12-14 15:19:42 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+#This program prints the name and meta data of a song
+
+SongName = "Devakanyaka"
+Genre = "Classical"
+Durationinseconds = 214
+NoOfWords = 42
+Tempo = 4.2
+Artist = "Yesudas"
+AlbumName = "Ee puzhayum kadannu"
+CoSinger = "Sujatha"
+Language = "Malayalam"
+print(SongName)
+print(Genre)
+print(Durationinseconds)
+print(NoOfWords)
+print(Tempo)
+print(Artist)
+print(AlbumName)
+print(CoSinger)
+print(Language)
+
