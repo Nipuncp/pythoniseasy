987def44bb59d4b234b8e0ccd3d3fc65ea03d516
Nipuncp <40162053+Nipuncp@users.noreply.github.com>
2018-12-15 18:16:29 +0530

    Add files via upload

---
+++ functionAttributes.py
@@ -0,0 +1,13 @@
+def songName():
+    name = "Devakanyaka"
+    return name
+
+def genre():
+    g_Name = "Classical"
+    return g_Name
+
+def durationInSeconds():
+    d_sec = 354
+    return d_sec
+
+    
